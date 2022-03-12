708f2b9ab743f4463608a9612efb35fd03b63aee
Ayakakaaa <ayakafaulhaber@gmail.com>
2022-03-12 11:05:17 -0800

    created fetch-posts-bytag function to get api call response

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,33 @@
 require 'net/http'
 class PostsController < ApplicationController
+    # before_action :get_tags_params, only: [:index]
+    # before_action :get_direction_params
+    # before_action :get_sortBy_params
     def index
-        render json: {"success": "success"}, status: 200
+        render json: fetch_posts_bytag, status: 200
     end
+
+    private
+
+    def fetch_posts_bytag
+        begin
+          uri = URI.parse("https://api.hatchways.io/assessment/blog/posts")
+          params = {tag: "tech"}
+          uri.query = URI.encode_www_form(params)
+          res = Net::HTTP.get_response(uri) 
+        rescue StandardError
+          false
+        end
+        if res.is_a?(Net::HTTPSuccess)
+          status = 200
+          result = res.body
+        else 
+          status = 400
+          result = {error: status}
+        end
+        result
+      end
+
+    
+
 end
