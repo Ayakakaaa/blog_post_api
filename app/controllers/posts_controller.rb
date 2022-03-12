ac9289d15ac2aa15c3102e24b496d45a7dcb7722
Ayakakaaa <ayakafaulhaber@gmail.com>
2022-03-12 11:20:03 -0800

    created fetch-post-bytags function to be able to get posts each tag

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,8 +3,11 @@ class PostsController < ApplicationController
     before_action :get_tags_params, only: [:index]
     before_action :get_direction_params
     before_action :get_sortBy_params
+
+     # GET /posts
     def index
-        render json: fetch_posts_bytag, status: 200
+        posts = fetch_posts_bytags
+        render json: posts, status: 200
     end
 
     private
@@ -26,6 +29,23 @@ class PostsController < ApplicationController
           result = {error: status}
         end
         result
+      end
+
+      #send request each time
+      def fetch_posts_bytags
+        posts = []
+        @tags_params.each do |tag|
+          posts_bytag = fetch_posts_bytag tag
+          posts = posts.concat(JSON.parse(posts_bytag)["posts"])
+          # ruby implements uniq efficiently using hash
+          posts = posts.uniq { |post| post["id"] }
+        end
+        if @direction_params == "asc"
+          # using ruby's efficient quicksort
+          posts.sort { |a,b| a[@sortBy_params] <=> b[@sortBy_params] }
+        else
+          posts.sort { |a,b| b[@sortBy_params] <=> a[@sortBy_params] }
+        end
       end
 
       def get_tags_params
