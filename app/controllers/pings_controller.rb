7bf8753f1e0c0f2a0e9ee6c9eb698fa8f4d79046
Ayakakaaa <ayakafaulhaber@gmail.com>
2022-03-12 10:50:51 -0800

    created routes and index action on Pings

--- app/controllers/pings_controller.rb
+++ app/controllers/pings_controller.rb
@@ -1,2 +1,7 @@
 class PingsController < ApplicationController
+
+    def index
+        status = 200
+        render json: {success: true}, status: status
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/api/ping", to: "pings#index"
+  get "/api/posts", to: "posts#index"
 end
